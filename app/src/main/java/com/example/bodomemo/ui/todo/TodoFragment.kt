c79ae77d819e895bea2767d4134be26d89898035
taratara10 <shuhei.knhs@gmail.com>
2021-01-18 17:32:01 +0900

    create todo item layout

---
+++ app/src/main/java/com/example/bodomemo/ui/todo/TodoFragment.kt
@@ -0,0 +1,31 @@
+package com.example.bodomemo.ui.todo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.bodomemo.R
+
+class TodoFragment : Fragment() {
+
+    private lateinit var todoViewModel: TodoViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        todoViewModel =
+                ViewModelProvider(this).get(TodoViewModel::class.java)
+        val root = inflater.inflate(R.layout.todo_activity, container, false)
+        val textView: TextView = root.findViewById(R.id.todoActivity)
+        todoViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/example/bodomemo/ui/todo/TodoViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.bodomemo.ui.todo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TodoViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
