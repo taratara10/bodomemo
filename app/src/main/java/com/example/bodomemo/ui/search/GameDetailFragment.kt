c92f6be1394b5a6058dbc5f085b46c9405f9790a
taratara10 <shuhei.knhs@gmail.com>
2021-02-01 20:06:05 +0900

    a

--- app/src/main/java/com/example/bodomemo/ui/search/GameDetailFragment.kt
+++ app/src/main/java/com/example/bodomemo/ui/search/GameDetailFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.example.bodomemo.R
+import com.example.bodomemo.data.db.GameEntity
 import com.example.bodomemo.ui.GameViewModel
+import kotlinx.android.synthetic.main.fragment_game_detail.view.*
 
 class GameDetailFragment: Fragment() {
     private lateinit var gameViewModel: GameViewModel
+    private var gameList: List<GameEntity> = arrayListOf()
+    private val args: SearchFragmentArgs by navArgs()
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -19,7 +27,12 @@ class GameDetailFragment: Fragment() {
     ): View? {
 
         gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_game_detail, container, false)
+        val root = inflater.inflate(R.layout.fragment_game_detail, container, false)
+
+        val gameId:Int = args.gameId
+        val gameList = gameViewModel.getAllGameList(1)
+        val gameRecord =
+        return root
     }
 
 }
