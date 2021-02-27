94abc41ebe39debfab8123370933e46820eb2baf
taratara10 <shuhei.knhs@gmail.com>
2021-02-27 18:48:31 +0900

    create calendar

--- app/src/main/java/com/example/bodomemo/ui/playHistory/PlayHistoryDetailFragment.kt
+++ app/src/main/java/com/example/bodomemo/ui/playHistory/PlayHistoryDetailFragment.kt
@@ -1,6 +1,9 @@
 package com.example.bodomemo.ui.playHistory
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,9 +11,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.bodomemo.R
+import com.example.bodomemo.data.db.GameEntity
 import com.example.bodomemo.data.db.PlayHistoryEntity
 import com.example.bodomemo.ui.PlayHistoryViewModel
+import kotlinx.android.synthetic.main.fragment_play_history_detail.*
 import kotlinx.android.synthetic.main.fragment_play_history_detail.view.*
+import java.util.*
 
 class PlayHistoryDetailFragment:Fragment() {
 
@@ -19,6 +25,8 @@ class PlayHistoryDetailFragment:Fragment() {
     private lateinit var playHistoryTitle: String
     private val playHistoryFragmentArgs: PlayHistoryFragmentArgs by navArgs()
     private val createNewPlayHistoryFragmentArgs: CreateNewPlayHistoryFragmentArgs by navArgs()
+
+    private var playHistoryDate = 0
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -33,10 +41,63 @@ class PlayHistoryDetailFragment:Fragment() {
         //navArgsのうちnullでないほうの値をセット
         val selectedPlayHistoryId = playHistoryFragmentArgs.playHistoryId?.toInt()
                 ?: createNewPlayHistoryFragmentArgs.createdPlayHistoryId?.toInt()
-                ?: throw Exception("cannot get gameId")
+                ?: throw Exception("cannot get playHistoryId")
 
         selectedPlayHistory = playHistoryViewModel.getPlayHistoryById(selectedPlayHistoryId)
+        playHistoryDate = selectedPlayHistory.date.toInt()
+
+        //setup layout
+        titleEditText.setText(selectedPlayHistory.title)
+        et_play_date_select_detail.setText(playHistoryDate.toString())
+
+
+        //editText
+        titleEditText.addTextChangedListener(object :CustomTextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                selectedPlayHistory.title = s.toString()
+                updatePlayHistory(selectedPlayHistory)
+            }
+        })
+
+        //DatePick
+        root.et_play_date_select_detail.setOnClickListener{
+            showDatePicker()
+        }
+
+
         return root
+    }
+
+
+
+
+
+
+    private fun showDatePicker() {
+        //todo milliSeconds -> Calender convert
+        val calender = Calendar.getInstance()
+        calender.timeInMillis = selectedPlayHistory.date
+        val year = calender.get(Calendar.YEAR)
+        val month = calender.get(Calendar.MONTH)
+        val day = calender.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+                requireActivity(),
+                DatePickerDialog.OnDateSetListener { view, y, m, d ->
+                    calender.set(y,m,d)
+                    playHistoryDate = calender.timeInMillis.toInt()
+
+                },year,month,day)
+        datePickerDialog.show()
+    }
+
+    fun updatePlayHistory(playHistoryEntity: PlayHistoryEntity) {
+        if (playHistoryTitle.trim().isNotEmpty()) playHistoryViewModel.updatePlayHistory(playHistoryEntity)
+    }
+
+    interface CustomTextWatcher: TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 
 }
