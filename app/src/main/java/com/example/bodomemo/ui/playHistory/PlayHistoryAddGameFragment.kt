b45f671ff1cf52fdec28b78ced781083618241d8
taratara10 <shuhei.knhs@gmail.com>
2021-03-03 21:51:35 +0900

    create insert crossRef function

--- app/src/main/java/com/example/bodomemo/ui/playHistory/PlayHistoryAddGameFragment.kt
+++ app/src/main/java/com/example/bodomemo/ui/playHistory/PlayHistoryAddGameFragment.kt
@@ -11,14 +11,18 @@ import android.widget.SimpleAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bodomemo.R
+import com.example.bodomemo.data.db.GameEntity
+import com.example.bodomemo.data.db.PlayAndGameCrossRef
 import com.example.bodomemo.data.db.PlayHistoryEntity
 import com.example.bodomemo.ui.GameViewModel
 import com.example.bodomemo.ui.PlayAndGameCrossRefViewModel
 import com.example.bodomemo.ui.search.SimpleListAdapter
 import com.example.bodomemo.ui.todo.AddTodoFragment
+import kotlinx.android.synthetic.main.fragment_create_new_game.*
 import kotlinx.android.synthetic.main.fragment_play_history_add_game.*
 import kotlinx.android.synthetic.main.fragment_play_history_add_game.view.*
 
@@ -71,7 +75,14 @@ class PlayHistoryAddGameFragment: Fragment(), SimpleListAdapter.GameAddEvents{
 
     //recyclerView item click events
     override fun onViewClicked(gameId: String?) {
-        TODO("Not yet implemented")
+        if(gameId != null) {
+            val newReference = PlayAndGameCrossRef(
+                    playHistoryId = playHistoryDetailFragmentArgs.playHistoryId!!.toInt(),
+                    gameId = gameId.toInt()
+                    )
+            playAndGameCrossRefViewModel.savePlayedGame(newReference)
+        }
+        parentFragmentManager.popBackStack()
     }
 
 }
