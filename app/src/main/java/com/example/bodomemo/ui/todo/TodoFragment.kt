508b1511d0224096478d8e87f789a679224b513f
taratara10 <shuhei.knhs@gmail.com>
2021-03-17 23:48:48 +0900

    disableSwipe

--- app/src/main/java/com/example/bodomemo/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/bodomemo/ui/todo/TodoFragment.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeRecyclerView
 import com.example.bodomemo.R
 import com.example.bodomemo.data.db.GameEntity
 import com.example.bodomemo.ui.GameViewModel
@@ -36,7 +37,11 @@ class  TodoFragment : Fragment(), DragTodoAdapter.TodoEvents {
         val todo_list = root.rv_drag_todo_list
         todo_list.layoutManager = LinearLayoutManager(activity)
         dragTodoAdapter = DragTodoAdapter(emptyList(), this)
-        todo_list?.adapter = dragTodoAdapter
+        todo_list.adapter = dragTodoAdapter
+
+        todo_list.orientation = DragDropSwipeRecyclerView.ListOrientation.VERTICAL_LIST_WITH_VERTICAL_DRAGGING
+        todo_list.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.RIGHT)
+        todo_list.disableSwipeDirection(DragDropSwipeRecyclerView.ListOrientation.DirectionFlag.LEFT)
 
         gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         gameViewModel.getTodoList().observe(viewLifecycleOwner, Observer {
